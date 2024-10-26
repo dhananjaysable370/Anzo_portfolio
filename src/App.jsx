21b89c1c5a9a81984ed2dcb209d2170a993bbc6b
Dhananjay <dhananjaysable370@gmail.com>
2024-10-26 14:09:54 +0530

    files added

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import Header from "./components/Header"
 import Page1 from "./pages/Page1"
 
 function App() {
 
   return (
     <>
+      <Header/>
       <Page1/>
     </>
   )
---
+++ src/components/Header.jsx
@@ -0,0 +1,11 @@
+import 'remixicon/fonts/remixicon.css'
+const Header = () => {
+  return (
+    <div className="absolute w-full flex z-10 items-center justify-end p-14">
+          <button className="bg-black px-10 py-3 rounded-full text-white border-4 hover:bg-gray-800 font-[anzo4] text-xs">Hire Me</button>
+          <i className="ri-more-2-fill"></i>
+    </div>
+  )
+}
+
+export default Header
